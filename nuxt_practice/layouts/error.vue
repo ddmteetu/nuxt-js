219140b79f6aee32505a5b4719520185a7ebfd1e
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-12 18:31:17 +0530

    creating a custom error page with its own layout

---
+++ nuxt_practice/layouts/error.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="error_page">
+    <li v-for="(value, key, index) in error" :key="index">
+      <b>{{key}}: </b>{{value}}
+    </li>
+    <hr>
+    <a href="/">Return to Home</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error_layout'
+  }
+</script>
+
+<style scoped>
+  #error_page {
+    border: 2px dashed blue;
+    border-radius: 5px;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    font-size: 22px;
+    line-height: 36px;
+  }
+
+  #error_page a {
+    font-size: 24px;
+    color: green;
+    text-decoration: none;
+  }
+
+  #error_page a:hover {
+    text-decoration: underline;
+  }
+</style>
---
+++ nuxt_practice/layouts/error_layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="error_layout">
+    <nuxt />
+  </div>
+</template>
+
+<style scoped>
+  #error_layout {
+    /* position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
