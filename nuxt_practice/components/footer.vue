edcdba3c2b2509aeff5f53e8c247f0a4906bdb77
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-12 17:41:01 +0530

    creating the 'header, main & footer' components

---
+++ nuxt_practice/components/footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="footer">
+    Footer by comp./footer.vue + layout/default.vue
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  #footer {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    height: 55px;
+    width: 100%;
+    font-size: 18px;
+    line-height: 55px;
+    text-align: center;
+    color: white;
+    background-color: green;
+  }
+</style>
---
+++ nuxt_practice/components/main.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="main"><slot /></div>
+</template>
+
+<style scoped>
+  #main {
+    padding-top: 60px;
+    padding-bottom: 55px;
+  }
+</style>
