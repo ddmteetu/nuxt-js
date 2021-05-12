c325b7a9aaaab70f6cb74572e21fa5c7e253a338
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-12 16:40:12 +0530

    creating custom error page

---
+++ nuxt_st/layouts/error.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="error">
+    <!-- <app-logo /> -->
+    <AppLogo />
+    <p id="errorStatus">{{error.statusCode}}</p>
+    <p id="errorMsg">{{error.message}}</p>
+  </div>
+</template>
+
+<script>
+  import AppLogo from '@/components/AppLogo'
+
+  export default {
+    props: ['error'],
+    components: {
+      AppLogo
+    }
+  }
+</script>
+
+<style>
+  #error {
+    margin: 20px;
+    text-align: center;
+    margin-top: 50%;
+  }
+
+  #errorStatus {
+    font-family: monospace;
+    font-size: 42px;
+    background-color: red;
+    color: white;
+    line-height: 60px;
+    margin: 10px 15%;
+  }
+
+  #errorMsg {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 24px;
+    line-height: 36px;
+  }
+</style>
