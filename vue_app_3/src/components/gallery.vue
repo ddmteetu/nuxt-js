2015c36116b4869da137e482287f62a8e9c4bd48
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-12 12:44:35 +0530

    Create the App Part 2

---
+++ vue_app_3/src/components/gallery.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="gallery">
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+</style>
--- vue_app_3/src/components/myComp.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div id="myComp">
-    <h3>My Component</h3>
-    <h4>URL: src/components/myComp.vue</h4>
-  </div>
-</template>
