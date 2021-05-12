afcf05c5a410d0ab9dd621a8f2a8cb3f07d73999
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-12 17:48:03 +0530

    connecting the 'header, main & footer' components to the layout, so that the components can be viewed in th UI

--- nuxt_practice/layouts/default.vue
+++ nuxt_practice/layouts/default.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
-    <Nuxt />
+    <Header />
+    <Main>
+      <Nuxt />
+    </Main>
+    <Footer />
   </div>
 </template>
+
+<script>
+  import Header from '@/components/header'
+  import Main from '@/components/main'
+  import Footer from '@/components/footer'
+
+  export default {
+    components: {
+      Header,
+      Main,
+      Footer
+    }
+  }
+</script>
 
 <style>
 html {
