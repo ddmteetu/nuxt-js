2cd95f1a177ef0d0d4ccbcf89c595e894109368c
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-13 15:03:29 +0530

    working with 'mutations' in Nuxt

--- nuxt_st/pages/vuex.vue
+++ nuxt_st/pages/vuex.vue
@@ -3,6 +3,9 @@
     {{$store.state.msg}}
     <hr />
     {{$store.state.module_1.msg}}
+    <hr />
+    <b-button @click="$store.commit('reset_msg', 'new msg value')">Reset MSG</b-button>
+    <b-button @click="$store.commit('module_1/reset_msg', 'new module one msg value')">Reset Module ONE MSG</b-button>
   </div>
 </template>
 
