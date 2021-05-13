2e5e6845ad865e5a5511676db3268a2dd4ee3ead
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-13 12:48:45 +0530

    Differences between 'assets' and 'static' files. 'Assets' files can not be linked dynamically using JS code. 'Static' files go directly into 'dist' folder and 'assets' files go under 'dist/_nuxt' folder and get renamed by 'webpack' when 'generate' runs

---
+++ nuxt_st/pages/resource.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="resource">
+    <img src="~/assets/asset.jpg" />
+    <hr />
+    <img src="/static.jpg" />
+    <!-- <hr />
+    <div id="frame"></div> -->
+    <hr />
+    <img :src="image[0]" />
+    <hr />
+    <img :src="image[1]" />
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        image: [
+          '~/assets/asset.jpg',
+          '/static.jpg'
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #resource {
+    text-align: center;
+  }
+
+  /* #frame {
+    margin: 0 auto;
+    width: 300px;
+    height: 255px;
+    background-image: url('/static.jpg'); using static files
+    background-image: url('~assets/asset.jpg'); using assets files (~/, @/, ~ only, but not @ only will work)
+  } */
+</style>
