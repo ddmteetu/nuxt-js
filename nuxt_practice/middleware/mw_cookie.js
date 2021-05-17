fdfb89a46c0892fb07eea76f15d6f15937c15580
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-17 17:28:00 +0530

    installing 'cookie-universal-nuxt' package to work with the cookies

--- nuxt_practice/middleware/mw_cookie.js
+++ nuxt_practice/middleware/mw_cookie.js
@@ -1,7 +1,11 @@
 export default function(context) {
-  if (process.server) {
-    console.log(context.req.headers.cookie)
-  } else {
-    console.log(document.cookie)
-  }
+  // if (process.server) { // to check the 'current environment', if it's server side or client side
+  //   console.log(context.req.headers.cookie)
+  // } else {
+  //   console.log(document.cookie)
+  // }
+  // const all = context.app.$cookies.getAll()
+  // console.log(all) // gives us 'object', cookies object
+  // console.log(all.signed) // to access property value using their names
+  // console.log(context.app.$cookies.get('signed')) // gives us the sigle property value, whose name is passed
 }
--- nuxt_practice/nuxt.config.js
+++ nuxt_practice/nuxt.config.js
@@ -35,7 +35,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    'bootstrap-vue/nuxt'
+    'bootstrap-vue/nuxt',
+    'cookie-universal-nuxt'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
