2524d77f1044ac7ac760d60a2f06e818a22d9363
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-17 14:29:39 +0530

    creating homepage and it's layout to list all cars and adding validate method to the parent page (_id.vue) to check the ID

---
+++ nuxt_practice/layouts/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header />
+    <Main>
+      <Nuxt />
+    </Main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+  import Header from '@/components/header'
+  import Main from '@/components/main'
+  import Footer from '@/components/footer'
+
+  export default {
+    components: {
+      Header,
+      Main,
+      Footer
+    }
+  }
+</script>
