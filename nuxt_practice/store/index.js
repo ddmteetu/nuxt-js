c8b3d49ec393c5744a9f58c615cb6f6670693be5
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-18 17:08:37 +0530

    in the VueX Store's 'nuxtServerInit' check the value for the first item of 'response.data', if it is 'true' then commit the mutation, else show the error message

--- nuxt_practice/store/index.js
+++ nuxt_practice/store/index.js
@@ -14,7 +14,13 @@ export const actions = {
   async nuxtServerInit(store, context) {
     try {
       const response = await context.$axios.get('http://localhost:9000/home')
-      store.commit('set_list', response.data)
+      if (response.data[0]) {
+        store.commit('set_list', response.data[1])
+      } else {
+        context.error({
+          message: 'database error...'
+        })
+      }
     } catch(e) {
       context.error({
         message: e.message
