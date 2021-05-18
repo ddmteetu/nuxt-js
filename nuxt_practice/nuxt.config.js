4247aaa9cb7aabd8f4d9b8b73551ae73a0246303
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-18 16:58:10 +0530

    using 'nuxtServerInit' action method to populate the homepage, by commiting 'set_list' mutation

--- nuxt_practice/nuxt.config.js
+++ nuxt_practice/nuxt.config.js
@@ -36,7 +36,8 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     'bootstrap-vue/nuxt',
-    'cookie-universal-nuxt'
+    'cookie-universal-nuxt',
+    '@nuxtjs/axios'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- nuxt_practice/store/index.js
+++ nuxt_practice/store/index.js
@@ -1,5 +1,24 @@
 export const state = () => ({
   list: [
-    
+
   ]
 })
+
+export const mutations = {
+  set_list(state, arg) {
+    state.list = arg
+  }
+}
+
+export const actions = {
+  async nuxtServerInit(store, context) {
+    try {
+      const response = await context.$axios.get('http://localhost:9000/home')
+      store.commit('set_list', response.data)
+    } catch(e) {
+      context.error({
+        message: e.message
+      })
+    }
+  }
+}
