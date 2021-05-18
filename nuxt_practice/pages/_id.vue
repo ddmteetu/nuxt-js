1e883fccd442c14f3400307e68660531ad530edf
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-18 17:33:44 +0530

    use the 'fetch' method to set the URL of the image directly into the store

--- nuxt_practice/pages/_id.vue
+++ nuxt_practice/pages/_id.vue
@@ -19,6 +19,19 @@
         // return false
         throw new Error('Illegal ID Value...')
       }
+    },
+    fetch(context) {
+      return context.$axios.get('http://localhost:9000/image/'+context.params.id)
+        .then((res) => {
+          if (res.data[0]) {
+            context.store.commit('set_image', [context.params.id, res.data[1]])
+          } else {
+            context.store.commit('set_image', [context.params.id, '/error.gif'])
+          }
+        })
+        .catch((err) => {
+          context.store.commit('set_image', [context.params.id, '/error.gif'])
+        })
     }
   }
 </script>
